05f12e141cdfcb1fd183d0381ec0fab9e54c0afe
Padmini <43469185+Padmini1330@users.noreply.github.com>
2020-06-07 21:48:55 +0530

    added search

---
+++ app/src/main/java/com/example/street_dancer_beta10/Segments/Search/Model/Users.java
@@ -0,0 +1,21 @@
+package com.example.street_dancer_beta10.Segments.Search.Model;
+
+public class Users {
+    public String name;
+
+    public Users(){
+
+    }
+
+    public Users(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
